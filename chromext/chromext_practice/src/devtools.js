683ac7259a51b7d04becf704c7f48d19fccbf1c6
weixy <weixiny2008@gmail.com>
2015-01-27 17:22:27 +1300

    first commit

---
+++ chromext/chromext_practice/src/devtools.js
@@ -0,0 +1,11 @@
+/**
+ * Created by y981821 on 27/01/15.
+ */
+
+chrome.devtools.panels.create("Image Search",
+    "ImageSearchIcon.png",
+    "panel.html",
+    function(panel) {
+        //
+    }
+);
