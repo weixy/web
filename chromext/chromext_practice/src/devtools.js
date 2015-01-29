94b636c4117b3b8182b7022183c4283b23e63e11
weixy <weixiny2008@gmail.com>
2015-01-29 17:32:35 +1300

    try to add message process code.

--- chromext/chromext_practice/src/devtools.js
+++ chromext/chromext_practice/src/devtools.js
@@ -6,6 +6,15 @@ chrome.devtools.panels.create("Image Search",
     "ImageSearchIcon.png",
     "panel.html",
     function(panel) {
-        //
+        var port = chrome.runtime.connect({name: "chromext_practice"});
+        port.onMessage.addListener(function(msg) {
+            console.log(msg);
+        });
+
+        panel.onShown.addListener(function lstn(win) {
+            panel.onShown.removeListener(lstn);
+            _window = win;
+            _window.port = port;
+        });
     }
 );
--- chromext/chromext_practice/src/panel.js
+++ chromext/chromext_practice/src/panel.js
@@ -1,3 +1,12 @@
 /**
  * Created by j1mw3i on 28/01/15.
  */
+
+function init() {
+    var queryBtn = document.querySelector('#query-button');
+    queryBtn.addEventListener('click', onQueryBtnClick);
+}
+
+function onQueryBtnClick() {
+    port.postMessage({xpath: "Test"});
+}
